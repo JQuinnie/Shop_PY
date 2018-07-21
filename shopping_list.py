180d21ed130c26844de873f7f530bf73d40d01ab
JQuinnie <jenn8866@gmail.com>
2018-07-21 10:29:25 -0600

    Add HELP as user prompt

--- shopping_list.py
+++ shopping_list.py
@@ -5,9 +5,13 @@ Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 """)
 
-
+show_help()
 while True:
     new_item = input("> ")
 
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
+        # continue continues on the iteration
+        continue
