a64680c47e908406f53a5a9102d06be82025da01
JQuinnie <jenn8866@gmail.com>
2018-07-21 10:31:48 -0600

    Make prompt pretty

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,13 @@
 def show_help():
-    print("What do you need to pick up at the stor?")
+    print('')
+    print('========================================')
+    print("What do you need to pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 """)
+    print('========================================')
+
 
 show_help()
 while True:
