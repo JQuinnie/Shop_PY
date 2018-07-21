038efd07a373f25a244ef2fd36fce0c598c87197
JQuinnie <jenn8866@gmail.com>
2018-07-21 16:09:01 -0600

    Add function to show list of items and new command to show list

--- shopping_list.py
+++ shopping_list.py
@@ -2,6 +2,19 @@
 shopping_list = []
 
 
+# create function for display HELP
+def show_help():
+    print('')
+    print('========================================')
+    print("What do you need to pick up at the store?")
+    print("""
+Enter 'SHOW' to show current list of items.
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+""")
+    print('========================================')
+
+
 # create a function named add_to_list that declares a parameter named item
 def add_to_list(item):
     # add the item to the list
@@ -11,16 +24,11 @@ def add_to_list(item):
         len(shopping_list)))
 
 
-# create function for display HELP
-def show_help():
-    print('')
-    print('========================================')
-    print("What do you need to pick up at the store?")
-    print("""
-Enter 'DONE' to stop adding items.
-Enter 'HELP' for this help.
-""")
-    print('========================================')
+# define a function named show_list that prints all the items in the list
+def show_list():
+    print("Here's your list:")
+    for item in shopping_list:
+        print("- " + item)
 
 
 show_help()
@@ -34,5 +42,11 @@ while True:
         show_help()
         # continue continues on the iteration
         continue
+    # enable the SHOW command to show the list
+    elif new_item == 'SHOW':
+        show_list()
+        continue
     # call add_to_list with new_item as an argument
     add_to_list(new_item)
+# show list on completion
+show_list()
