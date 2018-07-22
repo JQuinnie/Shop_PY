fbdd5a00dbdbc233611daaba50eade702261aaa0
JQuinnie <jenn8866@gmail.com>
2018-07-22 12:47:22 -0600

    Update list items with associated numbers

--- shopping_list.py
+++ shopping_list.py
@@ -15,32 +15,37 @@ def clear_screen():
 def show_help():
     clear_screen()
     print("")
-    print("========================================")
+    print("="*30)
     print("What do you need to pick up at the store?")
     print("""
 Enter 'SHOW' to show current list of items.
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 """)
-    print("========================================")
+    print("="*30)
 
 
 # create a function named add_to_list that declares a parameter named item
 def add_to_list(item):
+    show_list()
     # add the item to the list
     shopping_list.append(item)
     # notify user that the item was added, and state the number of items in the list currently
     print("Item added succesfully. There are currently {} items on the list.".format(
         len(shopping_list)))
-    show_list()
 
 
 # define a function named show_list that prints all the items in the list
 def show_list():
     clear_screen()
     print("Here's your list:")
+    # start index at 1 to add number to list items
+    index = 1
     for item in shopping_list:
-        print("- " + item)
+        print("{}. {}".format(index, item))
+        index += 1
+
+    print("-"*16)
 
 
 show_help()
